4fa79a253c202a76dcce01413e580406bc1de610
Park Haneul <parkhaneul0462@gmail.com>
2026-01-17 22:40:46 +0900

    25.01.18

--- cpp/sloved_ac/Bronze/2577.cpp
+++ cpp/sloved_ac/Bronze/2577.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	
+
 
 	int A, B, C;
 	cin >> A >> B >> C;
@@ -12,9 +12,9 @@ int main() {
 	int length = to_string(num).length(); // 루프하기 위한 값 변형
 	int value = 0; // 0부터 9까지 진행 시킬 조건 변수
 	string str = to_string(num); //ABC곱한 값 문자화
-	
+
 	for (int i = 0; i < 10; i++) {
-		char c = '0'+i;
+		char c = '0' + i;
 		int count = 0;
 		for (int j = 0; j < str.length(); j++) {
 			if (c == str[j]) {
@@ -23,5 +23,5 @@ int main() {
 		}
 		cout << count << endl;
 	}
-	
+
 }
